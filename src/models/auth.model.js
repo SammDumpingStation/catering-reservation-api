3e7840e3d012aaf5c15b5d1efbef0bbed8081a67
Rey Daug <daug.rey34@gmail.com>
2025-03-23 14:18:28 +0800

    👍 Added createAccount Model for Auth Controller.

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -1,21 +1,19 @@
 import Customer from "../schemas/customer.schema.js";
-import { encryptPassword, createToken } from "../utils/authUtils.js";
+import {
+  encryptPassword,
+  createToken,
+  createError,
+} from "../utils/authUtils.js";
 
-export const createAccount = async (fullName, email, password) => {
-  //Check if a customer already exists
-  const existingCustomer = await Customer.findOne({ email }); //utils
+export const createAccount = async ({ fullName, email, password }) => {
+  const existingCustomer = await Customer.findOne({ email });
+  if (existingCustomer) throw createError("Customer already exists", 400);
 
-  if (existingCustomer) {
-    const error = new Error("Customer already exists");
-    error.statusCode = 400; //400 ->  Bad Request
-    throw error;
-  }
+  const newCustomer = await Customer.create({
+    fullName,
+    email,
+    password: await encryptPassword(password),
+  });
 
-  //This is where we create the customer data that fits the schema we created in customer.model.js
-  const newCustomer = await Customer.create([
-    { fullName, email, password: await encryptPassword(password) },
-  ]);
-
-  //Create a session token for the customer for them to sign in
-  const token = createToken(newCustomer[0]._id);
+  return { token: createToken(newCustomer._id), customer: newCustomer };
 };
