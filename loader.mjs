b9ce8ac211fc4c8ddbeb660f0eb05561bcbbb1a9
Rey Daug <daug.rey34@gmail.com>
2025-03-24 02:22:35 +0800

    👍 Refactor project structure: update import paths, remove loader.mjs, and enhance nodemon configuration.

--- loader.mjs
+++
@@ -1,6 +0,0 @@
-import { register } from "node:module";
-import { pathToFileURL } from "node:url";
-
-register("ts-node/esm", pathToFileURL("./"));
-//REMEMBER TO REMOVE THIS AND NODEMON.json ONCE ALL FILES ARE CONVERTED TO TS
-//UNINSTALL THE NODEMON AND TS-NODE TOO
