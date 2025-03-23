2fd9b1746b69886cf1ef2e45c74ddb18d22f40a8
Rey Daug <daug.rey34@gmail.com>
2025-03-23 15:13:13 +0800

    👍 Added signInAccount models for Auth Controller.

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,6 +1,11 @@
 import Customer from "@schemas/customer.schema.js";
-import { encryptPassword, createToken, createError } from "@utils/authUtils.js";
-import { signUpProps } from "../types/auth.type.js";
+import {
+  encryptPassword,
+  createToken,
+  createError,
+  validatePassword,
+} from "@utils/authUtils.js";
+import { signInProps, signUpProps } from "../types/auth.type.js";
 
 export const createAccount = async ({
   fullName,
@@ -17,4 +22,14 @@ export const createAccount = async ({
   });
 
   return { token: createToken(newCustomer._id), customer: newCustomer };
+};
+
+export const signInAccount = async ({ email, password }: signInProps) => {
+  const customer = await Customer.findOne({ email });
+  if (!customer) throw createError("Customer not found", 404);
+
+  const isPasswordValid = await validatePassword(password, customer.password);
+  if (!isPasswordValid) throw createError("Invalid password", 401);
+
+  return { token: createToken(customer._id), customer };
 };
