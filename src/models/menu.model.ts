c10fae3c25ef9ed601da34fcb36c1cdf6b082431
Rey Daug <daug.rey34@gmail.com>
2025-03-24 14:25:40 +0800

    👍 Added updateMenuById service to Menu Models.

--- src/models/menu.model.ts
+++ src/models/menu.model.ts
@@ -1,8 +1,56 @@
 import Menu from "@schemas/menu.schema.js";
+import { MenuProps } from "@TStypes/menu.type.js";
 import { createError } from "@utils/authUtils.js";
 
 export const getMenuById = async (id: string) => {
   const existingMenu = await Menu.findById(id);
+
+  if (!existingMenu) throw createError("Menu doesn't exist!", 404);
+
+  return existingMenu;
+};
+
+export const updateMenyById = async ({
+  id,
+  name,
+  category,
+  available,
+  shortDescription,
+  fullDescription,
+  ingredients,
+  allergens,
+  preparationMethod,
+  prices,
+  regularPricePerPax,
+  imageUrl,
+  rating,
+  ratingCount,
+  spicy,
+  perServing,
+  nutritionInfo,
+}: MenuProps) => {
+  const existingMenu = await Menu.findByIdAndUpdate(
+    id,
+    {
+      name,
+      category,
+      available,
+      shortDescription,
+      fullDescription,
+      ingredients,
+      allergens,
+      preparationMethod,
+      prices,
+      regularPricePerPax,
+      imageUrl,
+      rating,
+      ratingCount,
+      spicy,
+      perServing,
+      nutritionInfo,
+    },
+    { new: true, runValidators: true }
+  );
 
   if (!existingMenu) throw createError("Menu doesn't exist!", 404);
 
