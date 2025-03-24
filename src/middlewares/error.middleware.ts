f12f96617dbfe43055ee7817dda774a353af0ee4
Rey Daug <daug.rey34@gmail.com>
2025-03-24 16:04:11 +0800

    👍 Refactor the Error Middleware from Javascript to Typescript.

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,58 @@
+import type {
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
+
+interface CustomError extends Error {
+  statusCode?: number;
+  code?: number;
+  errors?: Record<string, { message: string }>;
+}
+
+const errorMiddleware: ErrorRequestHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    let error = { ...err } as CustomError;
+    error.message = err.message;
+
+    console.error(err);
+
+    // Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      const message = "Resource not Found";
+      error = new Error(message) as CustomError;
+      error.statusCode = 404;
+    }
+
+    // Mongoose Duplicate key
+    if (err.code === 11000) {
+      const message = "Duplicate field value entered";
+      error = new Error(message) as CustomError;
+      error.statusCode = 400;
+    }
+
+    // Mongoose Validation Error
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors || {}).map(
+        (val) => (val as { message: string }).message
+      );
+      error = new Error(messages.join(", ")) as CustomError;
+      error.statusCode = 400;
+    }
+
+    // Don't return the response object
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message || "Server Error" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
