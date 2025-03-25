ec375e0f2d7156440fdc61d15c49e2f52794d155
Rey Daug <daug.rey34@gmail.com>
2025-03-25 10:47:10 +0800

    👍 Added authValidationRules for Auth.

---
+++ src/middlewares/validators/auth.validator.ts
@@ -0,0 +1,34 @@
+import { body } from "express-validator";
+import { validateRequest } from "@middlewares/validate-request.middleware.js";
+
+export const authValidationRules = {
+  signUp: [
+    body("fullName")
+      .isString()
+      .trim()
+      .isLength({ min: 2, max: 50 })
+      .withMessage("Full Name must be between 2 and 50 characters"),
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email address")
+      .normalizeEmail(),
+    body("password")
+      .isString()
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+    body("contactNumber").optional().isString().trim(),
+    body("role")
+      .optional()
+      .isIn(["customer", "caterer"])
+      .withMessage("Role must be either 'customer' or 'caterer'"),
+    validateRequest,
+  ],
+  signIn: [
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email address")
+      .normalizeEmail(),
+    body("password").isString().notEmpty().withMessage("Password is required"),
+    validateRequest,
+  ],
+};
