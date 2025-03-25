0990481a51c3a5d6089a51d9df9013dbd9157856
Rey Daug <daug.rey34@gmail.com>
2025-03-25 08:36:43 +0800

    👍 Added getCustomerReservations method to Reservation Controller.

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -1,5 +1,6 @@
 import Reservation from "@schemas/reservation.schema.js";
 import { NextFunction, Request, Response } from "express";
+import * as reservationModel from "@models/reservation.model.js";
 
 // Get all reservations (For Caterer)
 export const getAllReservations = async (
@@ -9,6 +10,23 @@ export const getAllReservations = async (
 ) => {
   try {
     const reservations = await Reservation.find();
+
+    res.status(200).json({ success: true, data: reservations });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get all reservations for a specific customer
+export const getCustomerReservations = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+
+    const reservations = await reservationModel.getReservationsByCustomerId(id);
 
     res.status(200).json({ success: true, data: reservations });
   } catch (error) {
