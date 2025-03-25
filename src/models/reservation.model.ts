b62419a2b46dd9cdc23f23a58928ded75cd8fc62
Rey Daug <daug.rey34@gmail.com>
2025-03-25 08:15:10 +0800

    👍 Added Unified Reservation Model and Controller.

---
+++ src/models/reservation.model.ts
@@ -0,0 +1,22 @@
+import Reservation from "@schemas/reservation.schema.js";
+import { createError } from "@utils/authUtils.js";
+import { checkIfExists } from "@utils/checkExistence.js";
+
+export const getReservationsByCustomerId = async (customerId: string) => {
+  const reservations = await Reservation.find({ customerId }).populate(
+    "customerId",
+    "fullName"
+  );
+
+  if (!reservations) throw createError("Reservations not found", 404);
+
+  return reservations;
+};
+
+export const getReservationById = async (id: string) => {
+  const reservation = await Reservation.findById(id);
+
+  if (!reservation) throw createError("Reservation not found", 404);
+
+  return reservation;
+};
