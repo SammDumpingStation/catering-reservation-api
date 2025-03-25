e6b2a7a8233376d754691095afefbe914228df35
Rey Daug <daug.rey34@gmail.com>
2025-03-25 09:34:46 +0800

    👍 Added getPaymentById service to Payment Model.

--- src/models/payment.model.ts
+++ src/models/payment.model.ts
@@ -1,8 +1,18 @@
 import Payment from "@schemas/payment.schema.js";
 import { PaymentProps } from "@TStypes/payment.type.js";
+import { createError } from "@utils/authUtils.js";
 
 // Create a new payment
 export const createPayment = async (paymentData: Partial<PaymentProps>) => {
   const payment = await Payment.create(paymentData);
+  return payment;
+};
+
+// Get payment by ID
+export const getPaymentById = async (id: string) => {
+  const payment = await Payment.findById(id).populate("reservationId");
+
+  if (!payment) throw createError("Payment not found", 404);
+
   return payment;
 };
